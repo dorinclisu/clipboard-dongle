b4845de0f69bd386a419df092aac69252a664b3a
dorin.clisu <dorin.clisu@gmail.com>
2026-01-29 18:52:14 +0200

    Enable replacement on web app paste, with backend storage for mapping

--- circuitpython/main.py
+++ circuitpython/main.py
@@ -1,13 +1,17 @@
+import digitalio
 import json
 import mdns
 import os
 import socketpool
+import time
 import usb_hid
 import wifi
 
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_httpserver import MIMETypes, Request, Response, Server, status
+
+import pins
 
 
 
@@ -18,6 +22,9 @@ srv_hostname = os.getenv('SERVER_HOSTNAME', 'clipboard-dongle')
 srv_port = int(os.getenv('SERVER_PORT', '5000'))
 
 ########################################################################################################################
+led = digitalio.DigitalInOut(pins.led_g)
+led.switch_to_output()
+
 keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard)
 
@@ -32,11 +39,10 @@ def load_replacements():
             yield key, val
 
 def save_replacements(replacements):
+    time.sleep(1)
     with open('/replace.csv', 'w') as f:
         for k, v in replacements.items():
             f.write(f'{k} {v}\n')
-
-char_replacements = dict(load_replacements())
 
 ########################################################################################################################
 with open('main.html', 'r') as f:
@@ -68,22 +74,46 @@ def submit(request: Request):
         text = request.body.decode('utf-8')
         print(f'Submitted: "{text[:64]}..."')
 
-        to_replace = set()
         for char in text:
-            if char in char_replacements:
-                to_replace.add(char)
-            elif ord(char) > 127:
+            if ord(char) > 127:
                 return Response(request, "Non-ASCII character: " + char, content_type="text/plain", status=status.BAD_REQUEST_400)
 
-        for char in to_replace:
-            text = text.replace(char, char_replacements[char])
-
+        led.value = True
         keyboard_layout.write(text)
+        led.value = False
         return Response(request, "OK", content_type="text/plain")
 
     except Exception as e:
         print(f"Error processing request: {e}")
-        return Response(request, "Error", content_type="text/plain", status=status.INTERNAL_SERVER_ERROR_500)
+        return Response(request, str(e), content_type="text/plain", status=status.INTERNAL_SERVER_ERROR_500)
+
+
+@server.route("/replacements", methods=["GET"])
+def get_replacements(request: Request):
+    try:
+        replacements = dict(load_replacements())
+        return Response(request, json.dumps(replacements), content_type="application/json")
+
+    except Exception as e:
+        print(f"Error processing request: {e}")
+        return Response(request, str(e), content_type="text/plain", status=status.INTERNAL_SERVER_ERROR_500)
+
+
+@server.route("/replacements", methods=["POST"])
+def update_replacements(request: Request):
+    try:
+        replacements = json.loads(request.body.decode('utf-8'))
+        for k, v in replacements.items():
+            for c in v:
+                if ord(c) > 127:
+                    return Response(request, f'Non-ASCII character in replacement for "{k}": ' + c,
+                                    content_type="text/plain", status=status.BAD_REQUEST_400)
+        save_replacements(replacements)
+        return Response(request, "OK", content_type="text/plain")
+
+    except Exception as e:
+        print(f"Error processing request: {e}")
+        return Response(request, str(e), content_type="text/plain", status=status.INTERNAL_SERVER_ERROR_500)
 
 ########################################################################################################################
 server.serve_forever(port=srv_port)
